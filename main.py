0e0afc5e0ebefc6ce34cd4abcd61ca502a25b893
freddybartlett <freddy.bartlett09@gmail.com>
2026-02-07 19:29:41 +0000

    using oop with tkinter to create the window for the application

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("test")
-print("test2")
+import tkinter as tk
+import sqlite3
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Banking application")
+        self.geometry("1280x720")
+
+app = App()
+app.mainloop()
